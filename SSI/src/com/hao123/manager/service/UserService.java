db08f139c2019f11a17383ea47bed9af8af6cbd2
zhaojielang <1406711233@qq.com>
2015-12-15 17:49:36 +0800

    111

--- SSI/src/com/hao123/manager/service/UserService.java
+++ SSI/src/com/hao123/manager/service/UserService.java
@@ -7,13 +7,29 @@ import org.springframework.cache.annotation.Cacheable;
 
 import com.hao123.manager.controller.user.form.UserForm;
 import com.hao123.manager.entity.custom.User;
+import com.hao123.manager.entity.help.ResponseDatagridMode;
 import com.hao123.manager.entity.help.ResponseModel;
 
 public interface UserService {
 	
+	/**
+	 * @param form
+	 * @return
+	 */
 	@Cacheable(value="user_ehcache", key="#form.id")
 	List<User> queryUserList(UserForm form);
+	
+	/**
+	 * @param form
+	 * @return
+	 */
 	@CacheEvict(value="user_ehcache",key="#form.id")
 	ResponseModel insertUser(UserForm form);
+	
+	/**
+	 * @param form
+	 * @return
+	 */
+	ResponseDatagridMode queryUserPaging(UserForm form);
 
 }
