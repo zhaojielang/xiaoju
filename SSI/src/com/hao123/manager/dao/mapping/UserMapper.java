1cf7cffdffe66677a6216201624acae1208e0204
zhaojielang <1406711233@qq.com>
2015-12-15 17:48:49 +0800

    代码生成器

---
+++ SSI/src/com/hao123/manager/dao/mapping/UserMapper.java
@@ -0,0 +1,17 @@
+package com.hao123.manager.dao.mapping;
+
+import com.hao123.manager.entity.mapping.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
---
+++ SSI/src/com/hao123/manager/dao/mapping/impl/UserMapperImpl.java
@@ -0,0 +1,38 @@
+package com.hao123.manager.dao.mapping.impl;
+
+import com.hao123.manager.dao.mapping.UserMapper;
+import com.hao123.manager.entity.mapping.User;
+
+public class UserMapperImpl implements UserMapper {
+
+	public int deleteByPrimaryKey(Long id) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int insert(User record) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int insertSelective(User record) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public User selectByPrimaryKey(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int updateByPrimaryKeySelective(User record) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int updateByPrimaryKey(User record) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
